4d744e60b89fb727dfde4e3c74ba6c5a4c5f0954
Brian "Beej Jorgensen" Hall <beej@beej.us>
2017-03-19 11:10:47 -0700

    1.17 solution

---
+++ sicp-1.17.scm
@@ -0,0 +1,34 @@
+; recursive from book
+;(define (* a b)
+;  (if (= b 0)
+;    0
+;    (+ a (* a (- b 1)))))
+
+; solution
+(define (double a) (* a 2))
+(define (halve a) (/ a 2))
+
+; keep the numeber of a's in a, b, and c invariant
+(define (mult a b)
+  (define (mult-iter a b c)
+    (cond ((= b 0) c)
+          ((even? b) (mult-iter (double a) (halve b) c))
+          (else (mult-iter a (- b 1) (+ c a)))))
+  (mult-iter a b 0))
+
+; test versus actual
+(define (test a b)
+  (if (not (= (mult a b) (* a b)))
+    (begin
+      (display a) (display " x ") (display b) (display " = ")
+      (display (mult a b))
+      (display ", should be: ")
+      (display (* a b))
+      (newline))))
+
+; Test 12x12 multiplication table
+(define (rows y)
+  (define (cols x)
+    (cond ((< x 13) (test y x) (cols (+ x 1)))))
+  (cond ((< y 13) (cols 0) (rows (+ y 1)))))
+(rows 0)
