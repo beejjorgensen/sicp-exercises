5f740e034771e181e51b90c973fd2fe133a8fc10
Brian "Beej Jorgensen" Hall <beej@beej.us>
2017-03-19 11:27:25 -0700

    Oops. Did 18 as 17. Now both correct.

--- sicp-1.18.scm
+++ sicp-1.18.scm
@@ -8,11 +8,13 @@
 (define (double a) (* a 2))
 (define (halve a) (/ a 2))
 
-; solution
+; keep the number of a's in a, b, and c invariant
 (define (mult a b)
-  (cond ((= b 0) 0)
-        ((even? b) (mult (* 2 a) (/ b 2)))
-        (else (+ a (mult a (- b 1))))))
+  (define (mult-iter a b c)
+    (cond ((= b 0) c)
+          ((even? b) (mult-iter (double a) (halve b) c))
+          (else (mult-iter a (- b 1) (+ c a)))))
+  (mult-iter a b 0))
 
 ; test versus actual
 (define (test a b)
